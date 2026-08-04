ee593e650ec67c73ed77d2e598bf91e50215602a
Manish Jon <41133660+manishjon6@users.noreply.github.com>
2026-08-04 12:54:47 +0530

    Update whethe_app.py

--- Manish_Bhatnagar_raw.py
+++ Manish_Bhatnagar_raw.py
@@ -2,7 +2,7 @@ import requests
 import os
 from datetime import datetime
 
-api_key = '285c52bdd298d32615b1f4cf4e6531cc'
+api_key = '285c52bdd298d32615b1f4cf4e6531cc0000'
 location = input("Enter the city name: ")
 
 complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
